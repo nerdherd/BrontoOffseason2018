a7ec4f3974574abe8f80c93faf1009b411fe7a3a
Dylan Barva <d.barva5402@gmail.com>
2018-11-28 08:15:49 -0800

    simpler trajectory following, pure pursuit needs more simming

--- src/main/java/com/team687/frc2018/commands/drive/auto/DriveTrajectory.java
+++ src/main/java/com/team687/frc2018/commands/drive/auto/DriveTrajectory.java
@@ -7,71 +7,44 @@
 
 package com.team687.frc2018.commands.drive.auto;
 
-import java.io.File;
-
 import com.team687.frc2018.Robot;
-import com.team687.frc2018.constants.DriveConstants;
+import com.team687.frc2018.utilities.TrajectoryFollower;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
-import jaci.pathfinder.followers.DistanceFollower;
-import jaci.pathfinder.modifiers.TankModifier;
 
 public class DriveTrajectory extends Command {
-  private Trajectory m_leftTrajectory, m_rightTrajectory, m_sourceTrajectory;
-  private DistanceFollower m_leftFollower, m_rightFollower;
-  private double m_leftOutput, m_rightOutput, m_turn, m_angularError;
-  private TankModifier m_modifier;
+  
+  private TrajectoryFollower m_controller;
+  private double m_leftVelocity, m_rightVelocity, m_startTime, m_time, m_lastTime;
 
-  public DriveTrajectory(Trajectory traj) {
-    m_sourceTrajectory = traj;
-    m_modifier = new TankModifier(m_sourceTrajectory);
-    m_modifier.modify(DriveConstants.kDrivetrainWidth);    
-    m_leftTrajectory = m_modifier.getLeftTrajectory();
-    m_rightTrajectory = m_modifier.getRightTrajectory();
+  public DriveTrajectory(Trajectory traj, int lookahead, Boolean goingForwards, double kP, double kD) {
+    m_controller = new TrajectoryFollower(traj, lookahead, goingForwards, kP, kD);
     requires(Robot.drive);
-  }
-
-  public DriveTrajectory(String file) {
-    File traj = new File("/home/lvuser/paths/" + file + "_source.traj");
-    SmartDashboard.putBoolean("Source exists", true);
-    m_sourceTrajectory = Pathfinder.readFromFile(traj);
-    File leftTraj = new File("/home/lvuser/paths/" + file + "_left.traj");
-    SmartDashboard.putBoolean("Left exists", true);
-    m_leftTrajectory = Pathfinder.readFromFile(leftTraj);
-    File rightTraj = new File("/home/lvuser/paths/" + file + "_right.traj");
-    SmartDashboard.putBoolean("Right exists", true);
-    m_rightTrajectory = Pathfinder.readFromFile(rightTraj);
-    
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    m_leftFollower = new DistanceFollower(m_leftTrajectory);
-    m_rightFollower = new DistanceFollower(m_rightTrajectory);
-    m_leftFollower.configurePIDVA(DriveConstants.kLeftVelocityP, 0, DriveConstants.kLeftVelocityD, DriveConstants.kLeftV, 0);
-    m_rightFollower.configurePIDVA(DriveConstants.kRightVelocityP, 0, DriveConstants.kRightVelocityD, DriveConstants.kRightV, 0);
+    m_startTime = Timer.getFPGATimestamp();
+    m_lastTime = Timer.getFPGATimestamp();
+    m_time = Timer.getFPGATimestamp() - m_startTime;
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    m_leftOutput = m_leftFollower.calculate(Robot.drive.getLeftPositionFeet()) + DriveConstants.kLeftStatic;
-    m_rightOutput = m_rightFollower.calculate(Robot.drive.getRightPositionFeet()) + DriveConstants.kRightStatic;
-    m_angularError = Pathfinder.boundHalfDegrees(Pathfinder.r2d(-m_leftFollower.getHeading()) - Robot.drive.getRawYaw());
-    m_turn = DriveConstants.kRotP * m_angularError;
-    Robot.drive.addDesiredVelocities(m_leftFollower.getSegment().velocity, m_rightFollower.getSegment().velocity);
-    Robot.drive.setPower(m_leftOutput + m_turn, m_rightOutput - m_turn);
-    SmartDashboard.putNumber("Velocity", m_leftFollower.getSegment().velocity);
+    m_time = Timer.getFPGATimestamp() - m_startTime;
+    m_controller.calculate(Robot.drive.getXpos(), Robot.drive.getYpos(), Robot.drive.getRawYaw(), m_time - m_lastTime);
+    Robot.drive.setVelocityFPS(m_controller.getLeftVelocity(), m_controller.getRightVelocity());
+    m_lastTime = m_time;
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return m_rightFollower.isFinished() && m_leftFollower.isFinished();
+    return m_controller.isFinished();
   }
 
   // Called once after isFinished returns true
